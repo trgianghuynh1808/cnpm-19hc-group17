2af0b011736eb1f286af1a7730aecb8857d58b81
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-03-08 16:31:12 +0700

    imp index layout and apply for current pages

--- frontend/src/containers/GalleryPage/GalleryPage.js
+++ frontend/src/containers/GalleryPage/GalleryPage.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../../components/Header';
-import PageTitle from '../../components/PageTitle';
-import Footer from '../../components/Footer';
-import Content from './components/Content';
+import React, { useState, useEffect } from "react";
+import PageTitle from "../../components/PageTitle";
+import Content from "./components/Content";
+import PageLayout from "../../layouts";
 
-import { getGallery } from '../../services/getListCar';
+import { getGallery } from "../../services/getListCar";
 
-  const GalleryPage = (props) => {
-    const [activePage, setActivePage] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-    const [carList, setCarList] = useState([]);
-    useEffect(() => {
-      const offset = activePage === 0 ? 0 : (activePage * 5 + activePage)  ;
-      const limit = offset + 5;
-      console.log(offset, limit)
-      getGallery(offset, limit).then((res)=> {
-        const { data = [], count = 0 } = res;
-        setPageCount(Math.ceil(count/6));
-        setCarList(data);
-      });
-    },[activePage]);
-    return (
-      <>
-        <Header/>
+const GalleryPage = props => {
+  const [activePage, setActivePage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [carList, setCarList] = useState([]);
+  useEffect(() => {
+    const offset = activePage === 0 ? 0 : activePage * 5 + activePage;
+    const limit = offset + 5;
+    console.log(offset, limit);
+    getGallery(offset, limit).then(res => {
+      const { data = [], count = 0 } = res;
+      setPageCount(Math.ceil(count / 6));
+      setCarList(data);
+    });
+  }, [activePage]);
+  return (
+    <>
+      <PageLayout>
         <PageTitle title="Our Gallery" />
-        <Content pageCount={pageCount} activePage={activePage} setActivePage={setActivePage} carList={carList}/>
-        <Footer/>
+        <Content
+          pageCount={pageCount}
+          activePage={activePage}
+          setActivePage={setActivePage}
+          carList={carList}
+        />
+      </PageLayout>
     </>
-    );
-  }
+  );
+};
 
 export default GalleryPage;
--- frontend/src/containers/HomePage/HomePage.js
+++ frontend/src/containers/HomePage/HomePage.js
@@ -1,41 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../../components/Header';
-import SearchForm from '../../components/SearchForm';
-import About from '../../components/About';
-import Partners from '../../components/Partners';
-import Services from '../../components/Services';
-import FunFact from '../../components/FunFact';
-import CarList from '../../components/CarList';
-import Testimonials from '../../components/Testimonials';
-import Articles from '../../components/Articles';
-import Footer from '../../components/Footer';
+import React, { useState, useEffect } from "react";
+import SearchForm from "../../components/SearchForm";
+import About from "../../components/About";
+import Partners from "../../components/Partners";
+import Services from "../../components/Services";
+import FunFact from "../../components/FunFact";
+import CarList from "../../components/CarList";
+import Testimonials from "../../components/Testimonials";
+import Articles from "../../components/Articles";
+import PageLayout from "../../layouts";
 
-import { getCarByBrands } from '../../services/getListCar';
+import { getCarByBrands } from "../../services/getListCar";
 
-  const HomePage = (props) => {
-    const [brandList, setBrandList] = useState([]);
+const HomePage = props => {
+  const [brandList, setBrandList] = useState([]);
 
-    useEffect(() => {
-      getCarByBrands().then((res)=>{
-        const { data = []} = res;
-        console.log(data);
-        setBrandList(data);
-      });
-    },[]);
-    return (
-      <>
-        <Header/>
+  useEffect(() => {
+    getCarByBrands().then(res => {
+      const { data = [] } = res;
+      console.log(data);
+      setBrandList(data);
+    });
+  }, []);
+  return (
+    <>
+      <PageLayout>
         <SearchForm />
-        <About/>
-        <Partners/>
-        <Services/>
-        <FunFact/>
+        <About />
+        <Partners />
+        <Services />
+        <FunFact />
         <CarList brandList={brandList} />
-        <Testimonials/>
-        <Articles/>
-        <Footer/>
+        <Testimonials />
+        <Articles />
+      </PageLayout>
     </>
-    );
-  }
+  );
+};
 
 export default HomePage;
