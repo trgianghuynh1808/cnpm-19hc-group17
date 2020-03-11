b71a22ed12ac588836a51a2be70bb96384b65522
baonh4 <1660031@gmail.com>
2020-03-11 21:57:55 +0700

    update new api flow

--- frontend/src/containers/CarDetailPage/CarDetailPage.js
+++ frontend/src/containers/CarDetailPage/CarDetailPage.js
@@ -1,36 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import Header from '../../components/Header';
 import PageTitle from '../../components/PageTitle';
 import Footer from '../../components/Footer';
 import Content from './components/Content';
 import queryString from 'query-string';
-import getCarDetails from '../../services/getCarDetails';
+import { getCarDetails, getCarDetailsSelector } from "../../stores/CarsState";
 
-  const CarDetailPage = (props) => {
-    const [ car, setCar ] = useState(null);
+
+const connectToRedux = connect(
+  createStructuredSelector({
+    carDetailsData: getCarDetailsSelector
+  }),
+  distpatch => ({
+    getCarDetails: carID => {
+      distpatch(getCarDetails(carID));
+    }
+  })
+);
+
+  const CarDetailPage = ({ getCarDetails, carDetailsData }) => {
     const [ isLoading, setIsLoading ] = useState(true);
     useEffect(() => {
-      const { location: { search } } = props;
+      const { location: { search } } = window;
       const queryObject = queryString.parse(search);
       const { id = 1 } = queryObject;
-      getCarDetails(id)
-      .then((data) => {
-        setCar(data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setCar(null);
-        setIsLoading(false);
-      })
+      getCarDetails(id);
+      setIsLoading(false);
     },[]);
+    console.log(carDetailsData);
     return (
       <>
         <Header/>
         <PageTitle title="Our Gallery" />
-        {!isLoading && <Content {...car} />}
+        {!isLoading && <Content {...carDetailsData} />}
         <Footer/>
     </>
     );
   }
-
-export default CarDetailPage;
+export default connectToRedux(CarDetailPage);
