d91c7f93e2ab9a9984b532c8f9d8e31002fdf1d4
trgianghuynh1808 <trgianghuynhdev1808@gmail.com>
2020-03-28 21:53:59 +0700

    Stopped tracking .env File

--- frontend/src/components/CarList/Title.js
+++ frontend/src/components/CarList/Title.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React from "react";
 
-const Title = (props) => {
-    return <div className="col-lg-12">
-    <div className="section-title  text-center">
-      <h2>Choose your Car</h2>
-      <span className="title-line"><i className="fa fa-car" /></span>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+const Title = () => {
+  return (
+    <div className="col-lg-12">
+      <div className="section-title  text-center">
+        <h2>Choose your Car</h2>
+        <span className="title-line">
+          <i className="fa fa-car" />
+        </span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default Title;
--- frontend/src/containers/HomePage/HomePage.js
+++ frontend/src/containers/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -29,7 +29,7 @@ const connectToRedux = connect(
 const HomePage = ({ getCarsByBrand, carByBrandsData }) => {
   useEffect(() => {
     getCarsByBrand();
-  }, []);
+  }, [getCarsByBrand]);
 
   if (!carByBrandsData) return <> </>;
 
