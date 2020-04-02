e5496f52f1537435cfe32bba4e6566c2d2285caf
baonh4 <1660031@gmail.com>
2020-04-02 21:11:54 +0700

    create UI

---
+++ admin/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WrapperCompnent from './components';
+
+function App() {
+  return (
+    <WrapperCompnent/>
+  );
+}
+
+export default App;
---
+++ admin/src/components/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Header';
+import SideBar from './SideBar';
+import Footer from './Footer';
+import Table from './Table';
+import Title from './Title';
+
+export default (props) => {
+  const { children } = props;
+    return (
+      <div className="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+        <Header/>
+        <div className="app-main">
+          <SideBar/>
+          <div className="app-main__outer">
+            <div className="app-main__inner">
+              <Title/>
+              <Table/>
+            </div>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    );
+}
