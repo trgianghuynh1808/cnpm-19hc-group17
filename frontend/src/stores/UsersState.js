8c9c9f03ac8156e15afa9f1b22e84a046777cd9e
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-04-05 22:07:18 +0700

    hot-fix: double UserState

--- frontend/src/stores/UsersState.js
+++ frontend/src/stores/UsersState.js
@@ -1,30 +1,55 @@
 import { makeFetchAction } from "redux-api-call";
-import { saveToken } from '../libs/token-libs';
-import { navigateWithClear } from '../utils/route';
 
+import { saveToken } from "../libs/token-libs";
+import { navigateWithClear } from "../utils/route";
 import { respondToSuccess } from "./middlewares/api-reaction";
 import nfetch from "../libs/nfetch";
 
 const LOGIN_API = "LOGIN_API";
+const CREATE_CONTRACT_USER_API = "CREATE_CONTRACT_USER_API";
 
-const loginAPI = makeFetchAction(LOGIN_API, ({ username, password }) =>{
+const loginAPI = makeFetchAction(LOGIN_API, ({ username, password }) => {
   return nfetch({
     endpoint: `/accounts/login`,
-  })({ username, password  })
-}
+  })({ username, password });
+});
 
-);
-
-export const login = ({username, password}) => {
-  return respondToSuccess(loginAPI.actionCreator({ username, password }), resp => {
-    if (resp.errors) {
-      console.error("Err: ", resp.errors);
+export const login = ({ username, password }) => {
+  return respondToSuccess(
+    loginAPI.actionCreator({ username, password }),
+    (resp) => {
+      if (resp.errors) {
+        console.error("Err: ", resp.errors);
+        return;
+      }
+      if (resp.token) {
+        saveToken(resp.token);
+        navigateWithClear("/");
+      }
       return;
     }
-    if(resp.token) {
-      saveToken(resp.token);
-      navigateWithClear('/');
+  );
+};
+
+const CreateContractUserAPI = makeFetchAction(
+  CREATE_CONTRACT_USER_API,
+  (objBody) =>
+    nfetch({
+      endpoint: `/contracts`,
+    })(objBody)
+);
+
+export const createContractUser = (objBody, callback) => {
+  return respondToSuccess(
+    CreateContractUserAPI.actionCreator(objBody),
+    (resp, _, store) => {
+      if (resp.errors) {
+        console.error("Err: ", resp.errors);
+        return;
+      }
+      typeof callback === "function" && callback(store.dispatch);
+
+      return;
     }
-    return;
-  });
+  );
 };
