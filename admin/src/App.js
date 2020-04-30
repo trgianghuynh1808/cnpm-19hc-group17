4225c483b8be41a3ed6b3e2f5bf54117e76d9243
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-04-30 20:00:33 +0700

    imp toast notify and fix using history

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import { Router } from 'react-router-dom';
-import App from './containers/App';
-import { createBrowserHistory } from 'history';
+import React, { Component } from "react";
+import { Router } from "react-router-dom";
+import App from "./containers/App";
+import { history } from "./utils/route";
 
-const history = createBrowserHistory();
 export default class extends Component {
   render() {
     return (
-    <Router history={history}>
-      <App/>
-    </Router>
+      <Router history={history}>
+        <App />
+      </Router>
     );
   }
 }
--- admin/src/utils/route.js
+++ admin/src/utils/route.js
@@ -1,26 +1,22 @@
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
 const navigateTo = (path, state = {}) => {
-    if (!history.location || history.location.pathname === path) {
+  if (!history.location || history.location.pathname === path) {
     return;
   }
-  
-    history.push(path, state);
-    window.scrollTo(0, 0);
-  };
 
-  const navigateWithClear = (path, state = {}) => {
-    if (!history.location || history.location.pathname === path) {
-      return;
-    }
-  
-    history.replace(path, state);
-    window.scrollTo(0, 0);
-  };
-export {
-    history,
-    navigateTo,
-    navigateWithClear
-}
+  history.push(path, state);
+  window.scrollTo(0, 0);
+};
+
+const navigateWithClear = (path, state = {}) => {
+  if (!history.location || history.location.pathname === path) {
+    return;
+  }
+  console.log("run here", history);
+  history.replace(path, state);
+  window.scrollTo(0, 0);
+};
+export { history, navigateTo, navigateWithClear };
