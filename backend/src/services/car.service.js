a12460d81adca0b158cafa8bd6f0c0969b0978c7
dungtruongtien <dungtruongtien411@gmail.com>
2020-05-17 09:53:05 +0700

    update car filter

--- backend/src/services/car.service.js
+++ backend/src/services/car.service.js
@@ -6,8 +6,10 @@ import { ResourceNotFoundError } from '../components/ErrorInstance/businessError
 export default class CarService {
     static list(params) {
         const obj = _.omit(params, ['limit', 'offset']);
-        const filterCondition = Object.keys(obj).map(key => ({ [key]: obj[key] }));
-        const whereCondition = filterCondition.length > 0 ? { $or: [...filterCondition] } : undefined;
+        let whereCondition = obj ? { ...obj } : undefined;
+        if (whereCondition.model) {
+            whereCondition = { ...whereCondition, model: { $like: `%${whereCondition.model}%` } };
+        }
         return db.Car.findAll({
             include: [{
                 model: db.Brand,
