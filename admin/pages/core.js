2488900b3d9854b1822ff636d4f14d30f7e02596
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-06-16 14:53:41 +0700

    init: core page

---
+++ admin/pages/core.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CorePage = () => {
+  return (
+    <div className="container">
+      <div className="row mt-2 justify-content-center">
+        <h3>This is core page</h3>
+      </div>
+    </div>
+  );
+};
+
+export default CorePage;
