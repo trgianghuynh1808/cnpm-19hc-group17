ff0fae408abf8534cc95a81ae5679b1524c35bf3
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-06-20 12:44:13 +0700

    init: manage bill page

---
+++ admin/components/rent/ManageBillPageComponent.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const ManageBillPageComponent = () => {
+  return (
+    <Fragment>
+      <div>This is manage bill page</div>
+      <style jsx>{``}</style>
+    </Fragment>
+  );
+};
+
+export default ManageBillPageComponent;
